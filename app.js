4318a99bf18156cb3777fab6021f0b17d6c347b3
Trainee 29 <umesh.more@raweng.com>
2021-02-02 23:12:36 +0530

    added schema

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { router } = require("./router/router");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
-const app = express();
-app.use(cors());
-app.use("/data", router);
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    const app = express();
+    app.use(cors());
+    app.use("/data", router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`On Port http://localhost:${process.env.PORT}/`);
-});
+    app.listen(process.env.PORT, () => {
+      console.log(`On Port http://localhost:${process.env.PORT}/`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+    return e;
+  });
---
+++ module/profileSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const uniquid = require("uniquid");
+const { profileData } = require("../controllers/getdata");
+
+const profileSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    default: uniquid(),
+  },
+
+  name: {
+    type: String,
+  },
+
+  email: {
+    type: String,
+  },
+
+  address: {
+    type: String,
+  },
+
+  phoneNo: {
+    type: String,
+  },
+
+  aboutMe: {
+    type: String,
+  },
+});
+
+const Profile = mongoose.model("Profile", profileData);
+
+module.exports.Profile = Profile;
