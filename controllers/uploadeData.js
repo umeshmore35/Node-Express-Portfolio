be5c6f0f96ed521c5eb7bd81213419fc8c88f4d4
Trainee 29 <umesh.more@raweng.com>
2021-02-03 15:20:00 +0530

    connected Schema

--- controllers/uploadeData.js
+++ controllers/uploadeData.js
@@ -1,6 +1,27 @@
+const { json } = require("express");
+const { Profile } = require("../module/profileSchema");
+
 createProfileData = async (req, res, next) => {
   console.log(req.body);
-  res.send("done");
+
+  const User = new Profile({
+    name: req.body.name,
+    email: req.body.email,
+    address: req.body.address,
+    phoneNo: req.body.phoneNo,
+    aboutMe: req.body.aboutMe,
+    imageUrl: req.body.imageUrl,
+  });
+
+  User.save()
+    .then((data) => {
+      console.log(data);
+      res.send("done");
+    })
+    .catch((e) => {
+      console.log(e);
+      return e;
+    });
 };
 
 module.exports.createProfileData = createProfileData;
--- module/profileSchema.js
+++ module/profileSchema.js
@@ -1,41 +1,40 @@
 const mongoose = require("mongoose");
 const uniquid = require("uniquid");
-const { profileData } = require("../controllers/getdata");
 
-const profileSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    default: uniquid(),
+const profileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      default: uniquid(),
+    },
+
+    name: {
+      type: String,
+    },
+
+    email: {
+      type: String,
+    },
+
+    address: {
+      type: String,
+    },
+
+    phoneNo: {
+      type: String,
+    },
+
+    aboutMe: {
+      type: String,
+    },
+
+    imageUrl: {
+      type: String,
+    },
   },
-
-  name: {
-    type: String,
-  },
-
-  email: {
-    type: String,
-  },
-
-  address: {
-    type: String,
-  },
-
-  phoneNo: {
-    type: String,
-  },
-
-  aboutMe: {
-    type: String,
-  },
-
-  imageUrl : {
-    type: String,
-  }
-
-}
   { timestamps: true }
 );
 
-const Profile = mongoose.model("Profile", profileData);
+const Profile = mongoose.model("Profile", profileSchema);
 
 module.exports.Profile = Profile;
