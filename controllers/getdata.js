e0081cb64a2c3fde09c52c598d3c2fe2235297e1
Trainee 29 <umesh.more@raweng.com>
2021-02-03 15:44:08 +0530

    added getdata middleware

--- controllers/getdata.js
+++ controllers/getdata.js
@@ -1,5 +1,25 @@
+const mongoose = require("mongoose");
+const { Profile } = require("../module/profileSchema");
+const selectQuery =
+  "userId name email address phoneNo aboutMe  imageUrl createdAt updatedAt -_id";
+
 const getProfileData = async (req, res, next) => {
-  res.send("hello");
+  try {
+    const tasknw = req.query.select;
+
+    if (tasknw) {
+      const task = await Task.find().select(`${tasknw} ${selectQuery}`);
+
+      return res.send(task);
+    } else {
+      const data = await Profile.find();
+
+      res.send(data);
+    }
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
 };
 
 module.exports.getProfileData = getProfileData;
